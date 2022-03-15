2f1d64d4d2d812c134c75b8ff62e2168bfd46a11
apodacaj1995 <contactjackiecodes@gmail.com>
2022-03-14 22:42:39 -0600

    html and js updated
    js not working

---
+++ script.js
@@ -0,0 +1,21 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+})
+function createTags(input){
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
+    tags.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEL = document.createElement('span')
+        tagEL.classList.add('tag')
+        tagEL.innerText = tag
+        tagsEl.appendChild(tagEL)
+    })
+}
